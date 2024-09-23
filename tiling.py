d4f6df9705e22eac011edee8b81efb8233956ba8
Han Bui <ha@sparkenv.com>
2024-09-24 01:07:33 +0200

    global detection

--- tiling.py
+++ tiling.py
@@ -14,8 +14,9 @@ class Tiling(dai.node.HostNode):
         self.x = None # vector [x,y] of the tile's dimensions
         self.tile_positions = []
         self.img_shape = None
+        self.global_detection = True
 
-    def build(self, overlap: float, img_output: dai.Node.Output, grid_size: tuple, grid_matrix, img_shape: tuple, nn_shape: int) -> "Tiling":
+    def build(self, overlap: float, img_output: dai.Node.Output, grid_size: tuple, grid_matrix, img_shape: tuple, nn_shape: int, global_detection: bool = True) -> "Tiling":
         self.sendProcessingToPipeline(True)
         self.link_args(img_output)
         self.overlap = overlap
@@ -23,15 +24,11 @@ class Tiling(dai.node.HostNode):
         self.grid_matrix = grid_matrix
         self.nn_shape = nn_shape
         self.img_shape = img_shape
+        self.global_detection = global_detection
         self.x = self._calculate_tiles(grid_size, img_shape, overlap)
         self._compute_tile_positions()
         
-        print(f"Tile dimensions: {self.x}")
         return self
-
-    def set_grid_matrix(self, grid_matrix: list) -> None:
-        self.grid_matrix = grid_matrix
-        self._compute_tile_positions()
 
     def process(self, img_frame) -> None:
         frame: np.ndarray = img_frame.getCvFrame()
@@ -48,20 +45,25 @@ class Tiling(dai.node.HostNode):
             tile_img_frame = self._create_img_frame(tile, img_frame, index, scaled_width, scaled_height)
             self.out.send(tile_img_frame)          
 
+    def _crop_to_nn_shape(self, frame: np.ndarray, scaled_width, scaled_height) -> np.ndarray:
+        if self.nn_shape is None:
+            raise ValueError("NN shape is not initialized.")
+        frame_resized = cv2.resize(frame, (scaled_width, scaled_height), interpolation=cv2.INTER_NEAREST)
+        frame_padded = np.zeros((self.nn_shape, self.nn_shape, 3), dtype=np.uint8)  # Initialize with zeros (black)
+
+        x_offset = (self.nn_shape - scaled_width) // 2 
+        y_offset = (self.nn_shape - scaled_height) // 2 
+        frame_padded[y_offset:y_offset+scaled_height, x_offset:x_offset+scaled_width] = frame_resized 
+
+        return frame_padded
+        
     def _create_img_frame(self, tile: np.ndarray, frame, tile_index, scaled_width, scaled_height) -> dai.ImgFrame:
         """
         Creates an ImgFrame from the tile, which is then sent to the neural network input queue.
         """
         if self.nn_shape is None:
             raise ValueError("NN shape is not initialized.")
-
-        tile_resized = cv2.resize(tile, (scaled_width, scaled_height), interpolation=cv2.INTER_NEAREST)
-
-        tile_padded = np.zeros((self.nn_shape, self.nn_shape, 3), dtype=np.uint8)  # Initialize with zeros (black)
-
-        x_offset = (self.nn_shape - scaled_width) // 2
-        y_offset = (self.nn_shape - scaled_height) // 2
-        tile_padded[y_offset:y_offset+scaled_height, x_offset:x_offset+scaled_width] = tile_resized
+        tile_padded = self._crop_to_nn_shape(tile, scaled_width, scaled_height) 
 
         planar_tile = to_planar(tile_padded, (self.nn_shape, self.nn_shape))
 
@@ -144,6 +146,17 @@ class Tiling(dai.node.HostNode):
                 components[comp_id].append((i, j))
 
         self.tile_positions = []
+        # add a whole image as a tile with index 0 (hence the first to go) 
+        if self.global_detection:
+            scale = min(self.nn_shape / img_width, self.nn_shape / img_height) 
+            scaled_width = int(img_width * scale)
+            scaled_height = int(img_height * scale)
+            self.tile_positions.append({
+                'coords': (0, 0, img_width, img_height),
+                'scaled_size': (scaled_width, scaled_height)
+            })
+        
+        # add the rest of the tiles
         for comp_id, positions in components.items():
             x1_list = []
             y1_list = []
